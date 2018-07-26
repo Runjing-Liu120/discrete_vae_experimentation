0a2f50a670551eeb7e751a936d9ec3c41e2211cd
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-07-26 18:14:09 +0000

    run overnight 7-25 on aws

--- cluster_scripts/run_mnist_training_test.sh
+++ cluster_scripts/run_mnist_training_test.sh
@@ -2,7 +2,7 @@
 
 python ../libraries/run_mnist_training.py \
 			--epochs 5 \
-			--save_every 11 \
+			--save_every 2 \
 			--batch_size 512 \
 			--seed 9033 \
 			--outfilename '../mnist_vae_results_aws/test' \
