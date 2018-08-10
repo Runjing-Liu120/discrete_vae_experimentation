fd36443bc61cae1d3d1c57a1859b7bf8cfc0bfed
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-08-10 21:31:48 +0000

    training scripts

--- cluster_scripts/run_mnist_training.sh
+++ cluster_scripts/run_mnist_training.sh
@@ -5,5 +5,5 @@ python ../libraries/run_mnist_training.py \
 			--save_every 10 \
 			--batch_size 256 \
 			--seed 9033 \
-			--outfilename '../mnist_vae_results_aws/mnist_vae_set_true_class' \
+			--outfilename '../mnist_vae_results_aws/mnist_vae5_set_true_class' \
 			--set_true_class_label True 	
