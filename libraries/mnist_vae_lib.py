22649d6de1cad318e93096416c917710536b9252
Runjing <runjing_liu@berkeley.edu>
2018-08-17 17:53:24 -0700

    different archetecture

--- libraries/mnist_vae_lib.py
+++ libraries/mnist_vae_lib.py
@@ -32,12 +32,9 @@ class MLPEncoder(nn.Module):
         self.n_classes = n_classes
 
         # define the linear layers
-        self.fc1 = nn.Linear(self.n_pixels + self.n_classes, 128) # 128 hidden nodes; two more layers
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, 128)
-        # self.fc4 = nn.Linear(128, self.n_pixels)
-        # self.fc5 = nn.Linear(self.n_pixels, latent_dim * 2)
-        self.fc4 = nn.Linear(128, latent_dim * 2)
+        self.fc1 = nn.Linear(self.n_pixels + self.n_classes, 256) # 128 hidden nodes; two more layers
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, latent_dim * 2)
 
     def forward(self, image, z):
 
@@ -47,8 +44,8 @@ class MLPEncoder(nn.Module):
 
         h = F.relu(self.fc1(h))
         h = F.relu(self.fc2(h))
-        h = F.relu(self.fc3(h))
-        h = self.fc4(h)
+        h = self.fc3(h)
+        # h = self.fc4(h)
         # h = self.fc5(h)
 
         # get means, std, and class weights
@@ -76,8 +73,8 @@ class Classifier(nn.Module):
         self.n_pixels = slen ** 2
         self.n_classes = n_classes
 
-        self.fc1 = nn.Linear(self.n_pixels, 128)
-        self.fc2 = nn.Linear(128, n_classes - 1)
+        self.fc1 = nn.Linear(self.n_pixels, 256)
+        self.fc2 = nn.Linear(256, n_classes - 1)
 
     def forward(self, image):
         h = image.view(-1, self.n_pixels)
@@ -104,10 +101,8 @@ class MLPConditionalDecoder(nn.Module):
         self.slen = slen
 
         self.fc1 = nn.Linear(latent_dim + n_classes, 128)
-        # self.fc2 = nn.Linear(self.n_pixels, 128)
-        self.fc3 = nn.Linear(128, 128)
-        self.fc4 = nn.Linear(128, 128)
-        self.fc5 = nn.Linear(128, self.n_pixels)
+        self.fc2 = nn.Linear(128, 256)
+        self.fc3 = nn.Linear(256, self.n_pixels)
 
         self.sigmoid = nn.Sigmoid()
 
@@ -119,10 +114,8 @@ class MLPConditionalDecoder(nn.Module):
         h = torch.cat((latent_params, z), dim = 1)
 
         h = F.relu(self.fc1(h))
-        # h = F.relu(self.fc2(h))
-        h = F.relu(self.fc3(h))
-        h = F.relu(self.fc4(h))
-        h = self.fc5(h)
+        h = F.relu(self.fc2(h))
+        h = self.fc3(h)
 
         h = h.view(-1, self.slen, self.slen)
 
