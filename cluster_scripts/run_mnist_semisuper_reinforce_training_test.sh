7f6f1dd71397b9160ba2e0329ea4333d64bcc01c
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-08-22 23:42:12 +0000

    testing script

--- cluster_scripts/run_mnist_semisuper_reinforce_training_test.sh
+++ cluster_scripts/run_mnist_semisuper_reinforce_training_test.sh
@@ -7,15 +7,15 @@ python ../libraries/run_mnist_semisupervised_training.py \
 			--propn_sample 0.005 \
 			--seed 901 \
 			--outfilename '../mnist_vae_results_aws/bernoulli_losses/mnist_vae2_semisuper_reinforce_warmstart' \
-			--alpha 1.0 \
+			--alpha 0.0 \
 			--propn_labeled 0.1 \
 			--reinforce True \
 			--latent_dim 32 \
 			--load_enc True \
 			--load_dec True \
-			--load_classifier False \
+			--load_classifier True \
 			--enc_init '../mnist_vae_results_aws/bernoulli_losses/mnist_vae2_semisuper_enc_final' \
 			--dec_init '../mnist_vae_results_aws/bernoulli_losses/mnist_vae2_semisuper_dec_final' \
-			--train_classifier_only True
-			# --classifier_init '../mnist_vae_results_aws/bernoulli_losses/mnist_vae2_semisuper_classifier_final' \
-			# --train_classifier_only True
+			--train_classifier_only True \
+			--classifier_init '../mnist_vae_results_aws/bernoulli_losses/mnist_vae2_semisuper_classifier_final'
+			--learning_rate 0.00001	
