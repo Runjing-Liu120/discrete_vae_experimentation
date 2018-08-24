1aab93f10c9302155a99dcd7a44f87ef1d797778
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-08-24 03:25:05 +0000

    cluster script

--- cluster_scripts/run_mnist_semisuper_reinforce_training_test.sh
+++ cluster_scripts/run_mnist_semisuper_reinforce_training_test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 python ../libraries/run_mnist_semisupervised_training.py \
-			--epochs 1000 \
+			--epochs 80 \
 			--save_every 100000 \
 			--batch_size 64 \
-			--propn_sample 0.005 \
+			--propn_sample 1.0 \
 			--seed 901 \
-			--outfilename '../mnist_vae_results_aws/foo' \
+			--outfilename '../mnist_vae_results_aws/reinforce_classifier_warmstart' \
 			--alpha 0.0 \
 			--propn_labeled 0.1 \
-			--reinforce True \
+			--reinforce False \
 			--use_baseline True \
 			--latent_dim 32 \
 			--load_enc True \
