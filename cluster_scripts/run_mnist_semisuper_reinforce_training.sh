df3bca680ef46d55a9866e5b2b12226fefcc5c14
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-08-28 18:50:01 +0000

    experiment training w new baseline

--- cluster_scripts/run_mnist_semisuper_reinforce_training.sh
+++ cluster_scripts/run_mnist_semisuper_reinforce_training.sh
@@ -2,7 +2,7 @@
 
 python ../libraries/run_mnist_semisupervised_training.py \
 			--epochs 200 \
-			--save_every 10 \
+			--save_every 50 \
 			--batch_size 256 \
 			--seed 901 \
 			--outfilename '../mnist_vae_results_aws/mnist_vae_semisuper_reinforce_trained_baseline' \
--- cluster_scripts/run_mnist_semisuper_reinforce_training_test.sh
+++ cluster_scripts/run_mnist_semisuper_reinforce_training_test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 python ../libraries/run_mnist_semisupervised_training.py \
-			--epochs 80 \
-			--save_every 100000 \
+			--epochs 200 \
+			--save_every 50 \
 			--batch_size 256 \
 			--propn_sample 1.0 \
 			--seed 901 \
-			--outfilename '../mnist_vae_results_aws/reinforce_classifier_warmstart' \
+			--outfilename '../mnist_vae_results_aws/reinforce_classifier_only' \
 			--alpha 0.0 \
 			--propn_labeled 0.1 \
-			--reinforce False \
+			--reinforce True \
 			--use_baseline True \
 			--latent_dim 32 \
 			--load_enc True \
@@ -17,6 +17,7 @@ python ../libraries/run_mnist_semisupervised_training.py \
 			--load_classifier False \
 			--enc_init '../saved_vae_results/mnist_vae_semisuper_enc_final' \
 			--dec_init '../saved_vae_results/mnist_vae_semisuper_dec_final' \
-			--classifier_init '../saved_vae_results/mnist_vae_semisuper_classifier_final'\
-                        --train_classifier_only True 
+			--classifier_init '../mnist_vae_results_aws/reinforce_classifier_warmstart_classifier_final'\
+                        --train_classifier_only True \
+			--learning_rate 1e-4
 
