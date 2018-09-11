39bbcb193313a26ab31a29af2b02e042060c5063
Runjing <runjing_liu@berkeley.edu>
2018-09-11 14:14:12 -0700

    implemented arm estimator

--- experimenting_arm/arm_experiments_lib.py
+++ experimenting_arm/arm_experiments_lib.py
@@ -14,6 +14,7 @@ import timeit
 from copy import deepcopy
 
 softmax = nn.Softmax(dim = 0)
+sigmoid = nn.Sigmoid()
 
 ## Losses
 
@@ -21,27 +22,26 @@ def get_loss(phi, p0):
     # loss for bernoulli experiments
     sum_p0 = torch.sum(p0**2)
     sum_1mp = torch.sum((1 - p0)**2)
-    sum_p = torch.Tensor([sum_p0, sum_1mp])
 
-    e_b = softmax(phi)
+    e_b = sigmoid(phi)
 
-    return torch.sum(sum_p * e_b)
+    return torch.sum(sum_p0 * (1 - e_b) + sum_1mp * e_b)
 
 def get_reinforce_ps_loss(phi, p0):
     # returns pseudoloss: loss whose gradient is unbiased for the
     # true gradient
 
     d = len(p0)
-    e_b = softmax(phi)
+    e_b = sigmoid(phi)
 
-    binary_samples = (torch.rand(d) > e_b[0]).float().detach()
+    binary_samples = (torch.rand(d) > e_b).float().detach()
 
     sampled_loss = torch.sum((binary_samples - p0)**2)
 
-    ps_loss = sampled_loss.detach() * torch.sum(
-                torch.log(e_b + 1e-8) * \
-                torch.Tensor([torch.sum(binary_samples),
-                              torch.sum(1 - binary_samples)]).detach())
+    sampled_log_q = torch.log(e_b + 1e-12) * torch.sum(binary_samples) + \
+                        torch.log(1 - e_b + 1e-12) * torch.sum(1 - binary_samples)
+
+    ps_loss = sampled_loss.detach() * sampled_log_q
 
     return ps_loss
 
@@ -51,8 +51,8 @@ def get_arm_ps_loss(phi, p0):
 
     d = len(p0)
 
-    sigma_phi = softmax(phi).detach()
-    sigma_neg_phi = softmax(-phi).detach()
+    sigma_phi = sigmoid(phi).detach()
+    sigma_neg_phi = sigmoid(-phi).detach()
 
     # sample uniform
     u = torch.rand(d)
@@ -62,13 +62,13 @@ def get_arm_ps_loss(phi, p0):
     z2 = (u < sigma_phi).float()
 
     # compute gradient:
-    f_delta = torch.sum((z_1 - p0)**2) - torch.sum((z_2 - p0)**2)
+    f_delta = torch.sum((z1 - p0)**2) - torch.sum((z2 - p0)**2)
     grad_f = f_delta * torch.sum(u - 0.5)
 
     return grad_f.detach() * phi
 
 ## Training functions
-def train_with_true_grad(phi0, p0, lr = 1.0, n_steps = 10000):
+def run_SGD(phi0, p0, get_ps_loss_fun, lr = 1.0, n_steps = 10000):
 
     init_loss = get_loss(phi0, p0)
 
@@ -78,49 +78,21 @@ def train_with_true_grad(phi0, p0, lr = 1.0, n_steps = 10000):
     optimizer = optim.SGD(params, lr = 1)
 
     loss_array = np.zeros(n_steps + 1)
-    phi_array = np.zeros((n_steps + 1, 2))
+    phi_array = np.zeros(n_steps + 1)
 
     loss_array[0] = init_loss.detach().numpy()
-    phi_array[0, :] = phi0.detach().numpy()
+    phi_array[0] = phi0.detach().numpy()
 
     for i in range(n_steps):
+        # run gradient descent
         optimizer.zero_grad()
-        loss = get_loss(phi, p0)
-
-        loss.backward()
-
-        optimizer.step()
-
-        loss_array[i + 1] = loss.detach().numpy()
-        phi_array[i + 1, :] = phi.detach().numpy()
-
-
-    return loss_array, phi_array
-
-def train_with_reinforce_grad(phi0, p0, lr = 1.0, n_steps = 10000):
-
-    init_loss = get_loss(phi0, p0)
-
-    # set up optimizer
-    phi = deepcopy(phi0)
-    params = [phi]
-    optimizer = optim.SGD(params, lr = 1)
-
-    loss_array = np.zeros(n_steps + 1)
-    phi_array = np.zeros((n_steps + 1, 2))
-
-    loss_array[0] = init_loss.detach().numpy()
-    phi_array[0, :] = phi0.detach().numpy()
-
-    for i in range(n_steps):
-        optimizer.zero_grad()
-        ps_loss = get_reinforce_ps_loss(phi, p0)
-
+        ps_loss = get_ps_loss_fun(phi, p0)
         ps_loss.backward()
-
         optimizer.step()
 
-        loss_array[i+1] = get_loss(phi.detach(), p0).numpy()
-        phi_array[i+1, :] = phi.detach().numpy()
+        # save losses
+        loss = get_loss(phi.detach(), p0)
+        loss_array[i + 1] = loss.detach().numpy()
+        phi_array[i + 1] = phi.detach().numpy()
 
     return loss_array, phi_array
