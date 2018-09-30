3ca8b2ea5a8fca5cfef793031dff9fda93818b34
Runjing <runjing_liu@berkeley.edu>
2018-09-30 14:10:42 -0700

    option for warm starts (need to test)

--- partial_marginalization_experiments/libraries/train_galaxy_vae.py
+++ partial_marginalization_experiments/libraries/train_galaxy_vae.py
@@ -44,6 +44,26 @@ parser.add_argument('--vae_outfilename', type = str,
 parser.add_argument('--save_every', type = int, default = 50,
                     help='save encoder ever how ___ epochs (default = 50)')
 
+# if warm start_time
+parser.add_argument('--galaxy_enc_warm_start', type = distutils.util.strtobool,
+                    default = False, help = 'whether to initialize the galaxy encoder')
+parser.add_argument('--galaxy_enc_init', type = str,
+                    default = '../galaxy_warm_starts/galaxy_enc_init.dat',
+                    help = 'file from which to load galaxy encoder')
+
+parser.add_argument('--galaxy_dec_warm_start', type = distutils.util.strtobool,
+                    default = False, help = 'whether to initialize the galaxy decoder')
+parser.add_argument('--galaxy_dec_init', type = str,
+                    default = '../galaxy_warm_starts/galaxy_dec_init.dat',
+                    help = 'file from which to load galaxy decoder')
+
+parser.add_argument('--attn_enc_warm_start', type = distutils.util.strtobool,
+                    default = False, help = 'whether to initialize the attention encoder')
+parser.add_argument('--attn_enc_init', type = str,
+                    default = '../galaxy_warm_starts/attn_enc_init.dat',
+                    help = 'file from which to load galaxy encoder')
+
+
 # seed
 parser.add_argument('--seed', type=int, default=64, metavar='S',
                     help='random seed (default: 64)')
@@ -57,13 +77,43 @@ np.random.seed(args.seed)
 def validate_args():
     assert os.path.exists(args.vae_outdir)
 
+    if args.attn_enc_warm_start:
+        assert os.path.isfile(args.attn_enc_init)
+
+    if args.galaxy_enc_warm_start:
+        assert os.path.isfile(args.galaxy_enc_init)
+
+    if args.galaxy_dec_warm_start:
+        assert os.path.isfile(args.galaxy_dec_init)
+
+
 validate_args()
 
 # get dataset
 ds = Synthetic(args.slen, min_galaxies=1, max_galaxies=1, mean_galaxies=1, num_images=12800)
 train_loader, test_loader = galaxy_lib.get_train_test_data(ds, batch_size=args.batchsize)
 
+# set up vae
 galaxy_vae = celeste_net.OneGalaxyVAE(args.slen)
+
+# if warm start
+if args.attn_enc_warm_start:
+    print('loading attention encoder from ' + args.attn_enc_init)
+    state_dict = torch.load(args.attn_enc_init, map_location='cpu')
+    galaxy_vae.attn_enc.load_state_dict(state_dict, strict=False)
+
+if args.galaxy_enc_warm_start:
+    print('loading galaxy encoder from ' + args.galaxy_enc_init)
+    state_dict = torch.load(args.galaxy_enc_init, map_location='cpu')
+    galaxy_vae.enc.load_state_dict(state_dict, strict=False)
+
+if args.galaxy_dec_warm_start:
+    print('loading galaxy decoder from ' + args.galaxy_dec_init)
+    state_dict = torch.load(args.galaxy_dec_init, map_location='cpu')
+    galaxy_vae.dec.load_state_dict(state_dict, strict=False)
+
+
+
 galaxy_rnn = galaxy_lib.CelesteRNN(args.slen, one_galaxy_vae=galaxy_vae)
 galaxy_rnn.cuda()
 
