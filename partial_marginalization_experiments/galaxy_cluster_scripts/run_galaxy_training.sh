942184f771918574649474ed7e8c51c1b0618d26
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-09-30 22:13:47 +0000

    fixes in argument names of training scripts

--- partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training.sh
+++ partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training.sh
@@ -2,8 +2,10 @@
 
 python ../libraries/train_galaxy_vae.py \
 			--epochs 1001 \
-			--save_every 50 \
+			--save_every 20 \
 			--batchsize 64 \
 			--seed 901 \
 			--vae_outdir '../galaxy_results/' \
-			--vae_outfilename 'galaxy_vae' 
+			--galaxy_enc_warm_start True \
+			--galaxy_dec_warm_start True \
+			--vae_outfilename 'galaxy_vae_warm_encdec_init' 
