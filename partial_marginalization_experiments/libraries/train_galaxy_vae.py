a45cc9bff7fe03eb6ea8d74bc5c142daa0cea53f
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-10-02 21:55:20 +0000

    original lrs

--- partial_marginalization_experiments/libraries/train_galaxy_vae.py
+++ partial_marginalization_experiments/libraries/train_galaxy_vae.py
@@ -144,7 +144,7 @@ galaxy_lib.train_module(galaxy_rnn, train_loader, test_loader,
                         topk = args.topk,
                         n_samples = args.n_samples,
                         use_baseline = True,
-                        lr = 1e-5,
+                        lr = 1e-4,
                         weight_decay = 1e-6,
                         filename = filename,
                         seed = args.seed)
