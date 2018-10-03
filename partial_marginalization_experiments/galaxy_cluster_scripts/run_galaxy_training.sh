286d77a19199bd42e56f8b08919d89b7b15eb2cd
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-10-03 03:41:13 +0000

    experimenting brightness and slne

--- partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training.sh
+++ partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training.sh
@@ -5,9 +5,9 @@ python ../libraries/train_galaxy_vae.py \
 			--save_every 10 \
 			--batchsize 64 \
 			--seed 904 \
-			--topk 0 \
-			--n_samples 6 \
-			--vae_outdir '../galaxy_results_sandbox/reinforce_nsamples6/' \
+			--topk 1 \
+			--n_samples 1 \
+			--vae_outdir '../galaxy_results_sandbox/topk1/' \
                         --vae_warm_start False \
-			--vae_outfilename 'galaxy_vae_reinforce_nsamples6' 
+			--vae_outfilename 'galaxy_vae_topk1' 
 
