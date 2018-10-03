a05c1082216636871a5205fbcfd9de5af0a4468c
Ubuntu <ubuntu@ip-172-31-1-23.us-west-2.compute.internal>
2018-10-03 03:40:33 +0000

    experimenting slen and brightness

--- partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training.sh
+++ partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 python ../libraries/train_galaxy_vae.py \
+			--slen 20 \
 			--epochs 201 \
 			--save_every 10 \
 			--batchsize 64 \
 			--seed 904 \
 			--topk 0 \
-			--n_samples 6 \
-			--vae_outdir '../galaxy_results_sandbox/reinforce_nsamples6/' \
+			--n_samples 4 \
+			--vae_outdir '../galaxy_results_sandbox/reinforce_nsamples4' \
                         --vae_warm_start False \
-			--vae_outfilename 'galaxy_vae_reinforce_nsamples6' 
+			--vae_outfilename 'galaxy_vae_reinforce_nsamples4' 
 
