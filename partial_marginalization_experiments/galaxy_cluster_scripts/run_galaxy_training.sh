b078b1792d137df4a3c5ff5196a417af54d25325
Ubuntu <ubuntu@ip-172-31-1-23.us-west-2.compute.internal>
2018-10-03 02:54:57 +0000

    experimenting brightness

--- partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training.sh
+++ partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 python ../libraries/train_galaxy_vae.py \
-			--epochs 501 \
+			--epochs 201 \
 			--save_every 10 \
 			--batchsize 64 \
 			--seed 904 \
-			--topk 5 \
-			--n_samples 1 \
-			--vae_outdir '../galaxy_results/topk5/' \
+			--topk 0 \
+			--n_samples 6 \
+			--vae_outdir '../galaxy_results_sandbox/reinforce_nsamples6/' \
                         --vae_warm_start False \
-			--vae_outfilename 'galaxy_vae_topk5' 
+			--vae_outfilename 'galaxy_vae_reinforce_nsamples6' 
 
