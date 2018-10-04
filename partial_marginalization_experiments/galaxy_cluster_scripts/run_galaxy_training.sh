8a041dd86e4da7877eb0556966e0bb73f3a6183e
Ubuntu <ubuntu@ip-172-31-1-23.us-west-2.compute.internal>
2018-10-04 03:57:50 +0000

    experiments

--- partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training.sh
+++ partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training.sh
@@ -7,7 +7,7 @@ python ../libraries/train_galaxy_vae.py \
 			--seed 904 \
 			--topk 0 \
 			--n_samples 4 \
-			--vae_outdir '../galaxy_results/testing_alwayson_reinforce4/' \
+			--vae_outdir '../galaxy_results/testing_star_proto_reinforce4/' \
                         --vae_warm_start False \
 			--vae_outfilename 'galaxy_vae_reinforce4' 
 
