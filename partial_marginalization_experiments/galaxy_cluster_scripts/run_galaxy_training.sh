f88e7ea8c09e18fef643ab17a8afbcda68faeeb4
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-10-16 18:39:36 +0000

    training-script

--- partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training.sh
+++ partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training.sh
@@ -7,7 +7,7 @@ python ../libraries/train_galaxy_vae.py \
 			--seed 904 \
 			--topk 1 \
 			--n_samples 1 \
-			--vae_outdir '../galaxy_results_final_experiments/topk1/' \
+			--vae_outdir '../galaxy_results_cv_experiments/topk1/' \
                         --vae_warm_start False \
 			--vae_outfilename 'galaxy_vae_topk1'\
 		
