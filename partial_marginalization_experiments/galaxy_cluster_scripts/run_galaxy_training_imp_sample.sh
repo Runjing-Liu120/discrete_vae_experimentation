e0c13a14c2e77eee348a3d06e958ba233f4613e1
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-10-16 22:19:23 +0000

    script

--- partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training_imp_sample.sh
+++ partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training_imp_sample.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 python ../libraries/train_galaxy_vae_imp_sampling.py \
-			--epochs 31 \
+			--epochs 51 \
 			--save_every 10 \
 			--batchsize 64 \
 			--seed 904 \
-			--use_importance_sample False \
+			--use_baseline False \
+			--use_importance_sample True \
 			--vae_outdir '../galaxy_results/imp_sampled/' \
 			--vae_outfilename 'galaxy_vae_imp_sampl_off'
+# 			--vae_outfilename 'galaxy_vae'
+
