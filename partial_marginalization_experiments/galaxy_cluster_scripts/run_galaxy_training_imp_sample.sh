3631f887e7a7051e16ccda2b206722f7312f9615
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-10-18 04:57:01 +0000

    baseline works with correct num detections

--- partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training_imp_sample.sh
+++ partial_marginalization_experiments/galaxy_cluster_scripts/run_galaxy_training_imp_sample.sh
@@ -6,8 +6,9 @@ python ../libraries/train_galaxy_vae_imp_sampling.py \
 			--batchsize 64 \
 			--seed 904 \
 			--max_detections 3\
-			--use_baseline False \
+			--use_baseline True \
 			--use_importance_sample False \
-			--vae_outdir '../galaxy_results/multiple_detections/' \
- 			--vae_outfilename 'galaxy_vae_3detections_imp_sample_off'
+			--vae_outdir '../galaxy_results/multiple_detections/' 
+  			--vae_outfilename 'galaxy_vae_3detections_bs_on_imp_sample_off'
+#                         --vae_outfilename 'galaxy_vae_3detections'
 
