875d95e6fe86d5315744508046138a0375b6a671
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-10-22 23:46:07 +0000

    experimenting w mnsit vae

--- mnist_experiments/cluster_scripts/run_mnist_semisuper_training.sh
+++ mnist_experiments/cluster_scripts/run_mnist_semisuper_training.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 python ../libraries/run_mnist_semisupervised_training.py \
-			--epochs 300 \
-			--save_every 50 \
+			--epochs 30 \
+			--save_every 10 \
 			--batch_size 64 \
 			--seed 901 \
 			--outdir '../mnist_vae_results/'\
 			--outfilename 'mnist_vae' \
 			--alpha 1.0 \
-			--propn_labeled 0.01 \
+			--propn_labeled 0.05 \
 			--propn_sample 1.0 \
-			--latent_dim 32 \
-			--learning_rate 1e-3
+			--latent_dim 50 \
+			--learning_rate 1e-2
