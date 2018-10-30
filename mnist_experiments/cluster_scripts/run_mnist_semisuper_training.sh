2ceaf46b895035130a6d4ddb00c17a6d9d4eeec6
Ubuntu <ubuntu@ip-172-31-9-102.us-west-2.compute.internal>
2018-10-30 17:44:58 +0000

    fix in print labeled accuracy

--- mnist_experiments/cluster_scripts/run_mnist_semisuper_training.sh
+++ mnist_experiments/cluster_scripts/run_mnist_semisuper_training.sh
@@ -6,12 +6,15 @@ python ../libraries/run_mnist_semisupervised_training.py \
 			--batch_size 256 \
 			--seed 901 \
 			--outdir '../mnist_vae_results/' \
-			--outfilename 'mnist_vae_true_labels' \
+			--outfilename 'mnist_vae_topk10' \
 			--alpha 1.0 \
 			--topk 10 \
 			--use_baseline False \
 			--propn_labeled 0.05 \
 			--propn_sample 1.0 \
-			--latent_dim 8 \
+			--latent_dim 4 \
 			--learning_rate 1e-2 \
-			--use_true_labels True
+			--use_true_labels False \
+                        --load_classifier True \
+                        --classifier_init '../mnist_vae_results/mnist_vae_true_labels_classifier_final'
+
