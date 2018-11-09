9250d89e3baa459373b487475bffb81e5a92cc93
Runjing <runjing_liu@berkeley.edu>
2018-11-08 18:09:42 -0800

    densenet for classifier

--- mnist_experiments/libraries/cifar_semisupervised_lib.py
+++ mnist_experiments/libraries/cifar_semisupervised_lib.py
@@ -27,14 +27,64 @@ from copy import deepcopy
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+# def get_cifar_semisuperivsed_vae(image_config = {'slen': 32,
+#                                                  'channel_num': 3,
+#                                                  'n_classes': 100},
+#                                 cond_vae_config = {'kernel_num': 128,
+#                                                    'z_size': 32},
+#                                 classifier_config = {'depth': 28,
+#                                                      'widen_factor': 10,
+#                                                      'dropout_rate': 0.3}):
+#
+#     cond_vae = CIFARConditionalVAE(slen = image_config['slen'],
+#                                     channel_num = image_config['channel_num'],
+#                                     kernel_num = cond_vae_config['kernel_num'],
+#                                     z_size = cond_vae_config['z_size'],
+#                                     n_classes = image_config['n_classes'])
+#
+#     classifier = Wide_ResNet(slen = image_config['slen'],
+#                              depth = classifier_config['depth'],
+#                              widen_factor = classifier_config['widen_factor'],
+#                              dropout_rate = classifier_config['dropout_rate'],
+#                              n_classes = image_config['n_classes'])
+#
+#     cifar_loglik = lambda image, image_mean, image_var: \
+#                             nn.BCELoss(size_average=False)(image_mean, image)
+#
+#     return ss_vae_lib.SemiSupervisedVAE(cond_vae, classifier, cifar_loglik)
+
+
+
+sys.path.insert(0, '../../../pytorch-cifar-models/models/')
+sys.path.insert(0, '../../../pytorch-cifar-models/')
+from models import densenet_cifar
+
+class MyClassifier(nn.Module):
+    def __init__(self, depth = 10, k = 12, n_classes = 100, slen = 28):
+        super(MyClassifier, self).__init__()
+
+        self.classfier = densenet_cifar.densenet_BC_cifar(depth = depth,
+                                                k = k,
+                                                num_classes = n_classes)
+
+        self.n_classes = n_classes
+        self.slen = slen
+
+        self.log_softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, image):
+        out = self.classfier(image)
+
+        return self.log_softmax(out)
+
+
 def get_cifar_semisuperivsed_vae(image_config = {'slen': 32,
                                                  'channel_num': 3,
                                                  'n_classes': 100},
                                 cond_vae_config = {'kernel_num': 128,
                                                    'z_size': 32},
-                                classifier_config = {'depth': 28,
-                                                     'widen_factor': 10,
-                                                     'dropout_rate': 0.3}):
+                                classifier_config = {'depth': 10,
+                                                    'k': 12}):
 
     cond_vae = CIFARConditionalVAE(slen = image_config['slen'],
                                     channel_num = image_config['channel_num'],
@@ -42,11 +92,10 @@ def get_cifar_semisuperivsed_vae(image_config = {'slen': 32,
                                     z_size = cond_vae_config['z_size'],
                                     n_classes = image_config['n_classes'])
 
-    classifier = Wide_ResNet(slen = image_config['slen'],
-                             depth = classifier_config['depth'],
-                             widen_factor = classifier_config['widen_factor'],
-                             dropout_rate = classifier_config['dropout_rate'],
-                             n_classes = image_config['n_classes'])
+    classifier = MyClassifier(depth = classifier_config['depth'],
+                                k = classifier_config['k'],
+                                n_classes = image_config['n_classes'],
+                                slen = image_config['slen'])
 
     cifar_loglik = lambda image, image_mean, image_var: \
                             nn.BCELoss(size_average=False)(image_mean, image)
